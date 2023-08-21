7bfbb17a57b4df89ee8646a016eca3781db76742
Mateusz Barwicki <72599764+Persiwal@users.noreply.github.com>
2023-08-21 16:34:46 +0200

    Create macy lib init file

---
+++ src/lib/macy/macy.js
@@ -0,0 +1,24 @@
+const createMacyImgGrid = (container) => {
+  const macyImgGrid = new Macy({
+    container: container,
+    mobileFirst: true,
+    columns: 1,
+    breakAt: {
+      600: 2,
+      1024: 3,
+    },
+    margin: {
+      x: 43,
+      y: 42,
+    },
+    wait: 500,
+  });
+
+  return macyImgGrid;
+};
+
+const recalculateMacyImgGrid = (macyInstance) => {
+  macyInstance.recalculate();
+};
+
+export { createMacyImgGrid, recalculateMacyImgGrid };
