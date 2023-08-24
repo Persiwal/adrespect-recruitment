87e45a6aee9163fdb97b43483e1c610c635bde85
Mateusz Barwicki <72599764+Persiwal@users.noreply.github.com>
2023-08-24 11:10:56 +0200

    Update scale function

--- src/components/main/about/about.js
+++ src/components/main/about/about.js
@@ -1,20 +1,25 @@
 import selectors from "../../../constants/selectors/selectors";
 
 const calculateScaleEffectForAboutImg = () => {
-  const distanceToViewportBottom =
-    selectors.parallaxContainer.getBoundingClientRect().bottom;
+  //apply only on desktop huge screen
+  if (window.innerWidth >= 1440) {
+    const distanceToViewportBottom =
+      selectors.parallaxContainer.getBoundingClientRect().bottom;
 
-  const distanceToViewportTop =
-    selectors.parallaxContainer.getBoundingClientRect().top;
+    const distanceToViewportTop =
+      selectors.parallaxContainer.getBoundingClientRect().top;
 
-  if (distanceToViewportBottom > 0 && distanceToViewportTop - 700 < 0) {
-    // Calculate the amount scrolled
-    const scrollY = window.scrollY;
+    if (distanceToViewportBottom > 0 && distanceToViewportTop - 700 < 0) {
+      // Calculate the amount scrolled
+      const scrollY = window.scrollY;
 
-    // Calculate the scale value for the parallax effect
-    let scale = 1.35 + scrollY * -0.00015; // Adjust the multiplier for desired zoom effect
+      // Calculate the scale value for the parallax effect
+      let scale = 1.35 + scrollY * -0.00015; // Adjust the multiplier for desired zoom effect
 
-    selectors.parallaxImage.style.transform = `scale(${scale})`;
+      selectors.parallaxImage.style.transform = `scale(${scale})`;
+    }
+  } else {
+    selectors.parallaxImage.style.transform = `scale(1)`;
   }
 };
 
